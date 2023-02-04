f66ae7aaef8dad92b7d59fd79d83af7444195bc5
Dan Poulsen <dangjogvara@gmail.com>
2023-02-04 19:53:58 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from models.person import Person
+
+dan = Person(1, 'Dan Poulsen', 'Comp. Sci.')
+durita = Person()
+
+print(dan)
---
+++ models/person.py
@@ -0,0 +1,42 @@
+class Person:
+    """Person class for the dbbook database."""
+
+    def __init__(self, id: int, name: str, dept_name: str):
+        """Initialize a Person object."""
+        self._id = id
+        self._name = name
+        self._dept_name = dept_name
+
+    @property
+    def id(self) -> int:
+        """Returns the id."""
+        return self._id
+
+    @id.setter
+    def id(self, id: int):
+        """Set the person id."""
+        self._id = id
+
+    @property
+    def name(self) -> str:
+        """Returns the name."""
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        """Set the name."""
+        self._name = name
+
+    @property
+    def dept_name(self) -> str:
+        """Get the department."""
+        return self._dept_name
+
+    @dept_name.setter
+    def dept_name(self, dept_name: str):
+        """Set the department."""
+        self._dept_name = dept_name
+
+    def __str__(self) -> str:
+        """Returns an object representation."""
+        return f"Id: {self.id}\nName: {self.name}\nDepartment: {self.dept_name}"
